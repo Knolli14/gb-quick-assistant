af1ac562033008ea3bd46c7e8bd1378f6040afad
Oliver Reichelt <olli.coding@proton.me>
2024-12-02 15:28:43 +0100

    added url

--- gb_assistant/params.py
+++ gb_assistant/params.py
@@ -3,3 +3,7 @@ import os
 RAW_DATA_PATH = os.path.join(
     os.path.dirname(__file__), "..", "raw_data"
 )
+
+SEARCH_PAGE_URL = "https://en.1jour-1jeu.com/rules/search?page="
+
+MAX_SEARCH_PAGE = 395
